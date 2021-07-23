3adc2c766fc7b0e22a56fec4602f0e4c21381856
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-07-23 16:18:26 -0300

    criando exceptions

---
+++ src/main/java/com/markusdel/apirestfullspringboot/exception/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.markusdel.apirestfullspringboot.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(Long id) {
+        super("Could not find product with id: " + id);
+    }
+}
