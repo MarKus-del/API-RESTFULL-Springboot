01c754a335e3900df75b852ec9dd25f4fb8202f4
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-07-23 15:37:41 -0300

    Começando projeto pelo spring initializr com dependências

---
+++ src/main/java/com/markusdel/apirestfullspringboot/ApirestFullSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.markusdel.apirestfullspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestFullSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestFullSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markusdel/apirestfullspringboot/ApirestFullSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markusdel.apirestfullspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestFullSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
