f48502099a58297705b6c0072aaf80a6075b2d63
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-07-23 16:16:44 -0300

    criado padrão dto para validação

---
+++ src/main/java/com/markusdel/apirestfullspringboot/dto/UpdateProductDTO.java
@@ -0,0 +1,31 @@
+package com.markusdel.apirestfullspringboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateProductDTO {
+
+    @NotNull
+    @NotBlank
+    @Size(min = 15, max = 60)
+    private String description;
+
+    @NotNull
+    @DecimalMax(value = "1000.00")
+    @DecimalMin(value = "0.30")
+    private BigDecimal price;
+
+    @NotNull
+    @Max(50)
+    @Min(1)
+    private Integer amount;
+}
