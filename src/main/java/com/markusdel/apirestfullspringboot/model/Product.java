ceb6865e21f37111d50f58e84d6d169ce5497959
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-07-23 16:15:11 -0300

    criado modelo de entidade

---
+++ src/main/java/com/markusdel/apirestfullspringboot/model/Product.java
@@ -0,0 +1,28 @@
+package com.markusdel.apirestfullspringboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "TB_PRODUTO")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer amount;
+
+}
